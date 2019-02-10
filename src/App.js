abcec255a0f702b8ab956fec384cadd212a54c18
AleksandraGontareva <a.gontareva@gmail.com>
2019-02-10 21:48:24 +0400

    Коммит инициализации

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Map-mapping is here!</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
