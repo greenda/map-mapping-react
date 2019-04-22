00fe9523d829c5c6ce4f34452af5b0a1646ed257
greenda <a.gontareva@gmail.com>
2019-04-22 23:12:53 +0400

    Добавлен контейнер карты

--- src/App.js
+++ src/App.js
@@ -3,13 +3,103 @@ import './App.css';
 import { connect } from 'react-redux';
 import { DragDropContextProvider } from 'react-dnd';
 import HTML5Backend from 'react-dnd-html5-backend';
+import * as d3 from 'd3'
 import  Tail from '../src/components/Tail';
-import Flight from './components/Flight';
 import { addTail } from './actions/pageActions';
 import Timeline from '../src/components/timeline/Timeline'
 import FlightList from '../src/components/flightList/FlightList'
 
 class App extends Component {
+  componentDidMount() {
+    console.log('componentDidMount')
+    d3.json('world_countries.json')
+      .then((countries) => {
+        console.log('countries')
+        this.showMapBackground(countries)
+      })
+      .catch(error => console.log(error))
+  }
+
+  showMapBackground(data) {
+    const width = 800
+    const height = 400
+    const svg = d3.select('#mapa')
+                  .attr("width", width)
+                  .attr("height", height)
+                  .attr('fill', 'green')
+                  .append('g')
+                  .attr('class', 'map')
+    const projection = d3.geoEquirectangular()
+                        .scale(130)
+                        .translate( [width / 2, height / 2])
+    const path = d3.geoPath().projection(projection)
+    svg.append("g")
+       .attr("class", "countries")
+       .selectAll("path")
+       .data(data.features)
+       .enter()
+       .append("path")
+       .attr("d", path)
+       .style('stroke', 'gray')
+       .style('stroke-width', 0.5)
+       .style('fill', 'white')
+
+    const points = [[3.22, 36.69], [77.96, 27.15], [18.6, -33.96]]
+
+    svg.selectAll('circle')
+       .data(points)
+       .enter()
+       .append('circle')
+       .attr('cx', (d) =>  projection(d)[0] )
+       .attr('cy', (d) => projection(d)[1])
+       .attr('r', '6px')
+       .attr('fill', '#ff9800')   
+       .attr('stroke', 'black')
+
+    const lines = [
+      {"type": "LineString", "coordinates": [[3.22, 36.69], [77.96, 27.15]], "id": 1},
+      {"type": "LineString", "coordinates": [[3.22, 36.69], [18.6, -33.96]], "id": 2 }
+    ]
+
+    svg.append("g")
+       .attr("class", "lines")
+       .selectAll("path")
+       .data(lines)
+       .enter()
+       .append("path")
+       .attr('class', (d) => 'line' + d.id)
+       .attr("d", path)
+       .attr('fill', 'none')   
+       .attr('stroke', 'black') 
+    
+    const line2 = svg.select('.line2').node()
+    const lineLenght = line2.getTotalLength()
+    const aircraft = line2.getPointAtLength(0.7 * lineLenght)
+    console.log(lineLenght)
+
+    svg.append("g")
+       .attr("class", "aircraft")
+       .selectAll('circle')
+       .data([[aircraft.x, aircraft.y]])
+       .enter()
+       .append('circle')
+       .attr('cx', (d) => d[0] )
+       .attr('cy', (d) => d[1])
+       .attr('r', '6px')
+       .attr('fill', 'red')   
+       .attr('stroke', 'black')
+  }
+
+  // translateAlong(path) {
+  //   var l = path.getTotalLength();
+  //   return (d, i, a) => {
+  //     return (t) = > {
+  //       const p = path.getPointAtLength(t * l);
+  //       return "translate(" + p.x + "," + p.y + ")";
+  //     };
+  //   };
+  // }
+
   render() {
     let array = [];
     for(let i = 0; i < this.props.tails.tails.length; i++) {
@@ -21,22 +111,11 @@ class App extends Component {
         </Tail>
       );
     }
-
-    // let flights = [];
-    // for(let i = 0; i < this.props.flights.flights.length; i++) {
-    //   let item = this.props.flights.flights[i]
-    //   flights.push(
-    //     <Flight id={item.id}
-    //             name={item.name}
-    //             tail={item.tail}>
-    //     </Flight>
-    //   );
-    // }
-
+  
     return (
       <DragDropContextProvider backend={HTML5Backend}>
       
-       <h1>Map-mapping is here!</h1>
+       <h4>Map-mapping is here!</h4>
        <div className="timeline__container">
         <Timeline />
        </div>
@@ -52,7 +131,10 @@ class App extends Component {
               <h2 className="flights__header">Flights</h2>
               <FlightList />
             </div>
-          </div>    
+          </div>   
+          <div>
+            <svg id="mapa" className="map__container"></svg>
+          </div> 
         </div>    
       </DragDropContextProvider>      
     );
--- src/selectors/index.js
+++ src/selectors/index.js
@@ -1,1 +1,67 @@
+import { createSelector } from 'reselect'
+
 export const currentTimeSelector = (state) => state.time.currentTime
+export const airportsSelector = (state) => state.airports
+export const airportIdsSelector = (state) => Object.keys(state.airports).map(value => +value)
+export const flightsSelector = (state) => state.flights
+export const flightIdsSelector = (state) => Object.keys(state.flights).map(value => +value)
+
+export const airportByIdSelector = createSelector(
+    airportsSelector,
+    (airports, id) => {
+        console.log('airportSelector')
+        return airports[id]
+    }
+)
+
+export const flightByIdSelector = createSelector(
+    currentTimeSelector,
+    airportsSelector,
+    flightsSelector,
+    (_, { id }) => id,
+    (currentTime, airports, flights, id) => {
+        console.log('flightByIdSelector')
+        const flight = {...flights[id]}
+        flight.fromIata = airports[flight.from].iata
+        flight.toIata = airports[flight.to].iata
+        const { dateTakeOff, dateLanding } = flight
+
+        // TODO В утилиту
+        switch(true) {
+            case (!dateTakeOff):
+                flight.progress = 0
+                flight.status = 'not takeOff'
+                break
+            case (!dateLanding):
+                flight.progress = 0
+                flight.status = 'not landing' 
+                break   
+            case (currentTime < dateTakeOff):
+                flight.progress = 0
+                // TODO статусы константами
+                flight.status = 'planed'
+                break
+            case (currentTime.isSame(dateTakeOff)):
+                flight.progress = 0
+                flight.status = 'takeOff'
+                break  
+            case (currentTime.isSame(dateLanding)):
+                flight.progress = 100
+                flight.status = 'landing'
+                break      
+            case (currentTime > dateLanding):
+                flight.progress = 100
+                flight.status = 'done'
+                break
+            case (dateTakeOff < currentTime && currentTime < dateLanding):  
+                flight.progress = currentTime.diff(dateTakeOff) / 
+                    dateLanding.diff(dateTakeOff) * 100 
+                flight.status = 'in progress'    
+                break
+            default: break;
+        }
+
+        return flight
+    }
+
+)
