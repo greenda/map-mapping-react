ab09d94ec5db71920e219d5467058954f1bab815
greenda <a.gontareva@gmail.com>
2019-08-14 11:44:27 +0400

    Updates

---
+++ precache-manifest.d04f3de0522ad0a4ef62ba793243aa0a.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "e8c5ef4869ef9b4b6c7ef9c1a2379a99",
+    "url": "/map-mapping-react/static/media/aircraft-icon__gray.e8c5ef48.svg"
+  },
+  {
+    "revision": "d498e4ac2a6aaaa31453",
+    "url": "/map-mapping-react/static/css/main.94812aea.chunk.css"
+  },
+  {
+    "revision": "d2e7f1e2493a60abb225",
+    "url": "/map-mapping-react/static/js/1.d2e7f1e2.chunk.js"
+  },
+  {
+    "revision": "3a5b264bd3e64f31d168",
+    "url": "/map-mapping-react/static/js/runtime~main.3a5b264b.js"
+  },
+  {
+    "revision": "40120811755db59228ee2395cc1ea25e",
+    "url": "/map-mapping-react/static/media/aircraft-icon__red.40120811.svg"
+  },
+  {
+    "revision": "93ced87adbff6debebcacf9cf4adebc2",
+    "url": "/map-mapping-react/static/media/achievement_Magelan.93ced87a.svg"
+  },
+  {
+    "revision": "1466a79770ea3b42392250c36ed1a334",
+    "url": "/map-mapping-react/static/media/achievement_avia_Croesus.1466a797.svg"
+  },
+  {
+    "revision": "3d9a7363f94eac280b807bff7ad683a6",
+    "url": "/map-mapping-react/static/media/achievement_our_hero.3d9a7363.svg"
+  },
+  {
+    "revision": "74bf1047f554fb0f15521dd8887a642b",
+    "url": "/map-mapping-react/static/media/achievement_great_armator.74bf1047.svg"
+  },
+  {
+    "revision": "f0ffb6aa1ccdad2e3afc2bd736b6479a",
+    "url": "/map-mapping-react/static/media/achievement_achievment_hunter.f0ffb6aa.svg"
+  },
+  {
+    "revision": "d498e4ac2a6aaaa31453",
+    "url": "/map-mapping-react/static/js/main.d498e4ac.chunk.js"
+  },
+  {
+    "revision": "0246aa141347217bd6c3b73cccdd3fc2",
+    "url": "/map-mapping-react/static/media/eurasia.0246aa14.png"
+  },
+  {
+    "revision": "cb6da07e5f085e99a7b29c4da295a51f",
+    "url": "/map-mapping-react/static/media/africa.cb6da07e.png"
+  },
+  {
+    "revision": "e4f8c1e3cd8fee6dc5358a87ac1f59fc",
+    "url": "/map-mapping-react/static/media/north_america.e4f8c1e3.png"
+  },
+  {
+    "revision": "2c73051657db53d7dc8df028485371f8",
+    "url": "/map-mapping-react/static/media/south_america.2c730516.png"
+  },
+  {
+    "revision": "50c226233ced3f258f7a47042b6e023f",
+    "url": "/map-mapping-react/static/media/australia.50c22623.png"
+  },
+  {
+    "revision": "499b78ece1251a9113c485a7f2859047",
+    "url": "/map-mapping-react/static/media/antarctica.499b78ec.png"
+  },
+  {
+    "revision": "73f27f6cf0116d227cef89fadbf43458",
+    "url": "/map-mapping-react/static/media/achievement_empty.73f27f6c.svg"
+  },
+  {
+    "revision": "098cb11e1082b645a78fa30bc0fa5c95",
+    "url": "/map-mapping-react/static/media/map-mapping-logo.098cb11e.svg"
+  },
+  {
+    "revision": "479f00a3c411265f3dfcbe9fc8c7f02d",
+    "url": "/map-mapping-react/index.html"
+  }
+];
